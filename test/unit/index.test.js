7a63f9ee2121c4b10e7f2b56763491ad4af81bd1
smlsunxie <smlsun@gmail.com>
2016-05-04 17:28:16 +0800

    update test

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,7 +1,7 @@
 
 import SystemAgentCore from '../../src';
 
-describe('systemAgentCore use OSX', () => {
+describe.only('systemAgentCore use OSX', () => {
 
   let systemAgentCore = null;
   beforeEach(() => {
@@ -52,58 +52,57 @@ describe('systemAgentCore use OSX', () => {
       done(e);
     }
   });
+});
 
-  describe('systemAgentCore use WINDOWS', () => {
+describe('systemAgentCore use WINDOWS', () => {
 
-    let systemAgentCore = null;
-    beforeEach(() => {
-      systemAgentCore = new SystemAgentCore({ostype: 'WINDOWS'});
-    });
-
-
-    it('should get OS info', async (done) => {
-
-      try {
-        let result = await systemAgentCore.getOSInfo();
-        result.should.has.keys('OSSoftwareData');
-
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
-
-    it.only('should get Browser info', async (done) => {
-
-      try {
-        let result = await systemAgentCore.getSoftwareInfo();
-
-        console.log(result);
-
-        result.should.has.keys('safari', 'chrome', 'flash');
-
-
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
-
-    it('should get Network info', async (done) => {
-
-      try {
-        let result = await systemAgentCore.getNetworkInfo();
-
-        console.log(result);
-
-        result.should.has.keys('networkSetup');
-
-
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
+  let systemAgentCore = null;
+  beforeEach(() => {
+    systemAgentCore = new SystemAgentCore({ostype: 'WINDOWS'});
   });
 
+
+  it('should get OS info', async (done) => {
+
+    try {
+      let result = await systemAgentCore.getOSInfo();
+      result.should.has.keys('OSSoftwareData');
+
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  it('should get Browser info', async (done) => {
+
+    try {
+      let result = await systemAgentCore.getSoftwareInfo();
+
+      console.log(result);
+
+      result.should.has.keys('safari', 'chrome', 'flash');
+
+
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  it('should get Network info', async (done) => {
+
+    try {
+      let result = await systemAgentCore.getNetworkInfo();
+
+      console.log(result);
+
+      result.should.has.keys('networkSetup');
+
+
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
 });

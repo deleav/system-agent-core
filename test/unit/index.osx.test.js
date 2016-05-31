3f1e449f5e3ddfeabed24ed0e8e3b2ac848bb917
FuYaoDe <dan82625@gmail.com>
2016-05-31 12:23:29 +0800

    fix test

--- test/unit/index.osx.test.js
+++ test/unit/index.osx.test.js
@@ -5,15 +5,14 @@ describe('systemAgentCore use OSX', () => {
 
   let systemAgentCore = null;
   beforeEach(() => {
-    systemAgentCore = new SystemAgentCore({ostype: 'OSX'});
-
+    systemAgentCore = new SystemAgentCore({ ostype: 'OSX' });
   });
 
 
   it('should get OS info', async (done) => {
 
     try {
-      let result = await systemAgentCore.getOSInfo();
+      const result = await systemAgentCore.getOSInfo();
       result.should.has.keys('OSSoftwareData');
       done();
     } catch (e) {
@@ -24,7 +23,7 @@ describe('systemAgentCore use OSX', () => {
   it('should get Browser info', async (done) => {
 
     try {
-      let result = await systemAgentCore.getSoftwareInfo();
+      const result = await systemAgentCore.getSoftwareInfo();
 
       console.log(result);
 
@@ -40,7 +39,7 @@ describe('systemAgentCore use OSX', () => {
   it('should get Network info', async (done) => {
 
     try {
-      let result = await systemAgentCore.getNetworkInfo();
+      const result = await systemAgentCore.getNetworkInfo();
 
       console.log(result);
 
@@ -56,29 +55,8 @@ describe('systemAgentCore use OSX', () => {
   it.skip('call teamview', async (done) => {
 
     try {
-      let teamviewPath = __dirname+'/../assets/osx/TeamViewerQS.app'
-      let result = await systemAgentCore.callTeamview({teamviewPath});
-
-      done();
-    } catch (e) {
-      done(e);
-    }
-  });
-});
-
-describe('systemAgentCore use WINDOWS', () => {
-
-  let systemAgentCore = null;
-  beforeEach(() => {
-    systemAgentCore = new SystemAgentCore({ostype: 'WINDOWS'});
-  });
-
-
-  it('should get OS info', async (done) => {
-
-    try {
-      let result = await systemAgentCore.getOSInfo();
-      result.should.has.keys('OSSoftwareData');
+      const teamviewPath = __dirname+'/../assets/osx/TeamViewerQS.app'
+      const result = await systemAgentCore.callTeamview({teamviewPath});
 
       done();
     } catch (e) {
@@ -86,48 +64,13 @@ describe('systemAgentCore use WINDOWS', () => {
     }
   });
 
-  it('should get Browser info', async (done) => {
-
+  it.only('get osx hardware info', async (done) => {
     try {
-      let result = await systemAgentCore.getSoftwareInfo();
-
+      const result = await systemAgentCore.getHardwareInfo();
       console.log(result);
-
-      result.should.has.keys('safari', 'chrome', 'flash');
-
-
       done();
     } catch (e) {
       done(e);
     }
   });
-
-  it('should get Network info', async (done) => {
-
-    try {
-      let result = await systemAgentCore.getNetworkInfo();
-
-      console.log(result);
-
-      result.should.has.keys('networkSetup');
-
-
-      done();
-    } catch (e) {
-      done(e);
-    }
-  });
-
-  it.skip('call teamview', async (done) => {
-
-    try {
-      let teamviewPath = __dirname+'//..//assets//windows//TeamViewerQS.exe'
-      let result = await systemAgentCore.callTeamview({teamviewPath});
-
-      done();
-    } catch (e) {
-      done(e);
-    }
-  });
-
 });

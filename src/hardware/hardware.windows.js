8205653b7f745dd77df2f421e15c4b29fbe1b1a4
FuYaoDe <--global>
2016-06-01 18:06:04 +0800

    format move to util

--- src/hardware/hardware.windows.js
+++ src/hardware/hardware.windows.js
@@ -1,48 +1,30 @@
 import { exec } from 'child-process-promise';
-import os from 'os';
-
-function formateWmic(str) {
-  const objRE = new RegExp(`\n(.*)`);
-  const match = str.match(objRE);
-  return match[1];
-}
-
-function formateWmicArray(str, key) {
-  const objRE = new RegExp(`${key}=(.*)\r`, 'g');
-  let match;
-  let matchArray = [];
-  while ((match = objRE.exec(str)) !== null) {
-    if (match.index === objRE.lastIndex) {
-      objRE.lastIndex++;
-    }
-    matchArray.push(match[1]);
-  }
-  return matchArray;
-}
+import networkService from '../network';
+import format from '../util/format';
 
 export async function getHardwareInfo() {
   try {
     const getCpuInfo = async () => {
       const cmd = 'wmic cpu get name';
       let result = await exec(cmd);
-      return formateWmic(result.stdout);
+      return format.formateWmic(result.stdout);
     };
 
     const getModelInfo = async () => {
       const cmd = 'wmic os get caption';
       let result = await exec(cmd);
-      return formateWmic(result.stdout);
+      return format.formateWmic(result.stdout);
     };
 
     const getRamlInfo = async () => {
       const getSizeCmd = 'wmic MEMORYCHIP get formfactor /VALUE';
       const getSizeResult = await exec(getSizeCmd);
-      const sizeArray =  formateWmicArray(getSizeResult.stdout, 'FormFactor');
+      const sizeArray =  format.formateWmicArray(getSizeResult.stdout, 'FormFactor');
 
       const getSpeedCmd = 'wmic MEMORYCHIP get Speed /VALUE';
       const getSpeedResult = await exec(getSpeedCmd);
-      const speedArray =  formateWmicArray(getSpeedResult.stdout, 'Speed');
-      
+      const speedArray =  format.formateWmicArray(getSpeedResult.stdout, 'Speed');
+
       const result = sizeArray.map((info, i) => {
         return {
           size: `${sizeArray[i]} GB`,
@@ -58,6 +40,7 @@ export async function getHardwareInfo() {
       model: await getModelInfo(),
       cpu: await getCpuInfo(),
       ram: await getRamlInfo(),
+      network: await networkService.WINDOWS.getNetworkHardwareInfo(),
     };
 
     return result;
--- src/network/network.windows.js
+++ src/network/network.windows.js
@@ -1,19 +1,40 @@
+import { exec } from 'child-process-promise';
+import format from '../util/format';
+
+export async function getNetworkHardwareInfo() {
+  const getNameCmd = 'wmic NIC get Name /VALUE';
+  const getNameResult = await exec(getNameCmd);
+  const nameArray =  format.formateWmicArray(getNameResult.stdout, 'Name');
+
+  const getAdapterTypeCmd = 'wmic NIC get AdapterType /VALUE';
+  const getAdapterTypeResult = await exec(getAdapterTypeCmd);
+  const adapterTypeArray =  format.formateWmicArray(getAdapterTypeResult.stdout, 'AdapterType');
+
+  const getMACAddressCmd = 'wmic NIC get MACAddress /VALUE';
+  const getMACAddressResult = await exec(getMACAddressCmd);
+  const macAddressArray =  format.formateWmicArray(getMACAddressResult.stdout, 'MACAddress');
+
+  const getNetEnabledCmd = 'wmic NIC get NetEnabled /VALUE';
+  const getNetEnabledResult = await exec(getNetEnabledCmd);
+  const netEnabledArray =  format.formateWmicArray(getNetEnabledResult.stdout, 'NetEnabled');
+
+  const result = nameArray.map((info, i) => {
+    return {
+      hardware: `${nameArray[i]} ${adapterTypeArray[i] || ''}`,
+      device: null,
+      ethernetAddress: macAddressArray[i] || null,
+      enable: !!netEnabledArray[i] || false,
+    }
+  });
+
+  return result;
+}
+
 export async function getNetworkInfo() {
   try {
-
-    let getNetworkSetup = async () => {
-      let cmd = '';
-      if(this.OSTYPE === 'OSX') cmd = 'networksetup -listallhardwareports';
-      else return ''
-
-      let result = await exec(cmd);
-      return result.stdout;
-    }
-
-    let result = {
-      networkSetup: await getNetworkSetup()
-    }
-
+    const result = {
+      networkSetup: await getNetworkHardwareInfo(),
+    };
     return result;
   } catch (e) {
     throw e;

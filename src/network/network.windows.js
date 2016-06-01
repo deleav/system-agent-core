8cad2f8981a2362e114467b27502bc675acdb2bc
FuYaoDe <--global>
2016-06-01 19:01:52 +0800

    fix error

--- src/network/network.windows.js
+++ src/network/network.windows.js
@@ -4,27 +4,10 @@ import format from '../util/format';
 async function wmic(tableName, typeName) {
   const cmd = `wmic ${tableName} get ${typeName} /VALUE`;
   const result = await exec(cmd);
-  const array = format.formateWmicArray(result.stdout, typeName);
-  return format.formateWmicArray(array);
+  return format.formateWmicArray(result.stdout, typeName);
 }
 
 export async function getNetworkHardwareInfo() {
-  // const getNameCmd = 'wmic NIC get Name /VALUE';
-  // const getNameResult = await exec(getNameCmd);
-  // const nameArray = format.formateWmicArray(getNameResult.stdout, 'Name');
-
-  // const getAdapterTypeCmd = 'wmic NIC get AdapterType /VALUE';
-  // const getAdapterTypeResult = await exec(getAdapterTypeCmd);
-  // const adapterTypeArray = format.formateWmicArray(getAdapterTypeResult.stdout, 'AdapterType');
-
-  // const getMACAddressCmd = 'wmic NIC get MACAddress /VALUE';
-  // const getMACAddressResult = await exec(getMACAddressCmd);
-  // const macAddressArray = format.formateWmicArray(getMACAddressResult.stdout, 'MACAddress');
-  //
-  // const getNetEnabledCmd = 'wmic NIC get NetEnabled /VALUE';
-  // const getNetEnabledResult = await exec(getNetEnabledCmd);
-  // const netEnabledArray = format.formateWmicArray(getNetEnabledResult.stdout, 'NetEnabled');
-
   const tableName = 'NIC';
   const [
     nameArray,
@@ -36,7 +19,7 @@ export async function getNetworkHardwareInfo() {
     wmic(tableName, 'AdapterType'),
     wmic(tableName, 'MACAddress'),
     wmic(tableName, 'NetEnabled'),
-  ])
+  ]);
 
   const result = nameArray.map((info, i) => {
     return {

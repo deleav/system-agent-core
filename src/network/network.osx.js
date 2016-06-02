7c7a76151770f9849f5b676110f2d15f8edb8525
hinx <b2765270@gmail.com>
2016-06-02 09:59:51 +0800

    ping

--- src/network/network.osx.js
+++ src/network/network.osx.js
@@ -1,30 +1,24 @@
-import { exec } from 'child-process-promise';
 import ping from 'net-ping';
 const session = ping.createSession();
 
 export async function getNetworkInfo() {
   try {
     const getNetworkSetup = async () => {
+      const target = '172.217.25.99';
 
-      const target = "172.217.25.99";
-
-      const result = await new Promise((done) => {
-        session.pingHost (target, function(error, target, sent, rcvd) {
-          console.log(target);
+      const result = new Promise((done) => {
+        session.pingHost(target, function(error, target, sent, rcvd) {
           const ms = rcvd - sent;
+
           if (error) {
-            // done(target + ": " + error.toString ());
+            done(`target: ${error.toString()}`);
           } else {
-            // done(target + ": Alive (ms=" + ms + ")");
+            done(`target: Alive (ms = ${ms})`);
           }
         });
       });
       console.log('getNetworkSetup', result);
       return result;
-      // return ms;
-      // const cmd = 'networksetup -listallhardwareports';
-      // const result = await exec(cmd);
-      // return result.stdout;
     };
 
     const result = {

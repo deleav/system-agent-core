ec8b921c6adc01575ce3706d4ebf5eecdc1cf417
FuYaoDe <dan82625@gmail.com>
2016-06-22 16:56:22 +0800

    add exportReport

--- src/config/index.js
+++ src/config/index.js
@@ -4,4 +4,5 @@ import downloads from './ftp.downloads';
 export default {
   ulConfig: uploads,
   dlConfig: downloads,
+  domain: 'http://127.0.0.1:1337',
 };
--- src/report.js
+++ src/report.js
@@ -1,4 +1,11 @@
+import axios from 'axios';
+import config from './config';
 export async function exportReport(json) {
-
-  return true;
+  try {
+    await axios.post(`${config.domain}/report`, json);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 }
